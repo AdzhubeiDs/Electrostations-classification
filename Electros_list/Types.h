013514a2bc4665cec8b4dc4bea723ca0591591fe
matveygovorukhin <113597503+matveygovorukhin@users.noreply.github.com>
2023-03-03 00:14:07 +0300

    Update Types.h
    добавил свои классы гидро, ветро и геотермо

--- Electros_list/Types.h
+++ Electros_list/Types.h
@@ -31,3 +31,35 @@ public:
         type_fuel = "Солнечная энергия";
     }
 };
+
+class Hydro: public Electros {
+protected:
+    string river;
+public:
+    Hydro() {
+        string type_fuel = "гидроресурс";
+        fuel = 5;
+    }
+//    Hydro(int power,string owner, string region, string river){
+//        this -> power = power;
+//        this -> owner = owner;
+//        this -> region = region;
+//        this -> river = river;
+    virtual void GetRiver(){}
+};
+
+class Wind: public Electros{
+public:
+    Wind() {
+        string type_fuel = "гидроресурс";
+        fuel = 5;
+    }
+};
+
+class Geother: public Electros{
+public:
+    Geother() {
+        string type_fuel = "гидроресурс";
+        fuel = 5;
+    }
+};
