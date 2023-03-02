76c0cbc176177d4c3710eb8cddda432cccc70f80
AdzhubeiDs <114070216+AdzhubeiDs@users.noreply.github.com>
2023-03-03 00:33:22 +0300

    Add files via upload

--- Electros_list/Termal_Nuclear_Solar.cpp
+++ Electros_list/Termal_Nuclear_Solar.cpp
@@ -1,22 +1,13 @@
-﻿// наследование.1.cpp: определяет точку входа для приложения.
-//
+﻿
 #include <iostream>
 #include "Electros.h"
 #include "Types.h"
-
 using namespace std;
-/// <summary>
-///     int power;
-///string region;
-///string owner;
-///int fuel;
-/// </summary>
-
-
 
 class Belgorodskaya : public Thermal {
 public:
 	Belgorodskaya(){
+        power = 60;
 		region = "Белгородская область";
 		owner = "ПАО 'Квадра'";
 	}
@@ -29,10 +20,97 @@ public:
     void GetOwner() {
         cout << owner << endl;
     }
-    void GetFuel() {
-        cout << fuel << endl;
+ 
+};
+
+class Vologda : public Thermal {
+public:
+    Vologda() {
+        power = 132.1;
+        region = "Вологодская область";
+        owner = "ПАО 'ТГК - 2'";
+    }
+    void GetPower() {
+        cout << power << endl;
+    }
+    void GetRegion() {
+        cout << region << endl;
+    }
+    void GetOwner() {
+        cout << owner << endl;
+    }
+    
+};
+
+class Kolskaya public Nuclear {
+public:
+    Kolskaya() {
+        power = 1840;
+        region = "Мурманская область";
+        owner = "АО 'Концерн Росэнергоатом'";
+    }
+    void GetPower() {
+        cout << power << endl;
+    }
+    void GetRegion() {
+        cout << region << endl;
+    }
+    void GetOwner() {
+        cout << owner << endl;
     }
 };
 
+class Rostov : public Nuclear {
+public:
+    Rostov() {
+        power = 4071;
+        region = "Ростовская область";
+        owner = "АО 'Концерн Росэнергоатом'";
+    }
+    void GetPower() {
+        cout << power << endl;
+    }
+    void GetRegion() {
+        cout << region << endl;
+    }
+    void GetOwner() {
+        cout << owner << endl;
+    }
 
+};
 
+class Arshanskaya : public Solar {
+public:
+    Arshanskaya() {
+        power = 115.6;
+        region = "Республика Калмыкия";
+        owner = "ПАО 'ФОРТУМ'";
+    }
+    void GetPower() {
+        cout << power << endl;
+    }
+    void GetRegion() {
+        cout << region << endl;
+    }
+    void GetOwner() {
+        cout << owner << endl;
+    }
+};
+
+class Samara : public Solar {
+public:
+    Samara() {
+        power = 75;
+        region = "Самарская область";
+        owner = "ООО 'Солар Систем'";
+    }
+    void GetPower() {
+        cout << power << endl;
+    }
+    void GetRegion() {
+        cout << region << endl;
+    }
+    void GetOwner() {
+        cout << owner << endl;
+    }
+};
