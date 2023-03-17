60c31a9e4687a0025a78d8e6ac1f4cffd0e65ab2
Arina Zholnerovich <114029905+ArinaZ18@users.noreply.github.com>
2023-03-17 12:37:06 +0300

    Update Tidal.h

---
+++ Electros_list/Tidal.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_CPP_TIDAL_H
+#define MAIN_CPP_TIDAL_H
+#include <string>
+#include "Electros.h"
+using namespace std;
+
+class Tidal: public Electros{
+public:
+    Tidar(string name, int power,string owner, string region_name);
+};
+
+#endif //MAIN_CPP_TIDAL_H
